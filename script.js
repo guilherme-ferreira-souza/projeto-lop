0d0e5612d989461a77f5818cc97c809b1b9db2ee
guilherme-ferreira-souza <80338511+guilherme-ferreira-souza@users.noreply.github.com>
2023-11-11 02:59:37 -0300

    d

--- script.js
+++ script.js
@@ -23,6 +23,12 @@ function draw(){
         fill('blue');
         rect(100, 250, 200, 50, 15); 
         fill('black'); 
+        text("Categorias", 160, 280);
+
+        //Botão 3
+        fill('blue');
+        rect(100, 350, 200, 50, 15); 
+        fill('black'); 
         text("Créditos", 160, 280);
     
         
@@ -30,7 +36,7 @@ function draw(){
     
         if(mouseX>=100 && mouseX<=(100+200)&& mouseY>=150 && mouseY<=(150+50)){ 
           
-          //verificando se o mouse foi precionsando no botão 1
+          //verificando se o mouse foi pressionado no botão 1
           
             if(mouseIsPressed){ 
                 tela=1; //atribuição  do valor 1 mudando para a tela 1
@@ -42,11 +48,23 @@ function draw(){
     
         if(mouseX>=100 && mouseX<=(100+200)&& mouseY>=250 && mouseY<=(250+50)){
           
-          //verificando se o mouse foi precionsando no botão 2
+          //verificando se o mouse foi pressionado no botão 2
           
             if(mouseIsPressed){ 
                 tela=2;
             }
+        }
+
+
+        //verificando se o mouse está na area do botão 3
+        
+        if(mouseX>=100 && mouseX<=(100+200)&& mouseY>=350 && mouseY<=(350+50)){
+          
+            //verificando se o mouse foi pressionado no botão 3
+
+            if(mouseIsPressed){ 
+                  tela=3;
+                }
         }
     }
     if(tela==1){
@@ -75,5 +93,26 @@ function draw(){
         textSize(17);
         fill('black'); //cor do texto
         text("texto dos creditos", 130 , 350);
+    }
+
+    if(tela==3){
+        background(230);   //cor de fundo
+        textAlign(LEFT);  //Alinhamento do texto a esquerda 
+        textSize(25)  //tamanho da tela
+    
+        fill('black'); 
+        text("Categorias", 100, 70);
+        
+        //Botão 1
+        fill('blue'); //cor do botão 
+        rect(100, 150, 200, 50, 15); //retângulo  do botão 
+        fill('black'); //cor do text
+        text("Categoria 1", 160, 180); //texto do botão 
+    
+        //Botão 2
+        fill('blue');
+        rect(100, 250, 200, 50, 15); 
+        fill('black'); 
+        text("Categoria 2", 160, 280);
     }
 }
